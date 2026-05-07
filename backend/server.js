4405b863ae2697c86877a834196b30b4ec0d7af2
markryan3421 <c20223192.zipagan@csav.edu.ph>
2026-05-07 10:04:03 +0800

    Backend setup.

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+// import express from 'express';
+const express = require("express");
+
+const app = express();
+
+// Routes
+app.get("/api/notes", (req, res) => {
+  res.send("Note fetched");
+});
+
+app.listen(5001, () => {
+  console.log("Connected");
+});
