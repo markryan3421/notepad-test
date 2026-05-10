e00a70e4a1065af6c9025adc899dda9a04252810
markryan3421 <c20223192.zipagan@csav.edu.ph>
2026-05-10 19:29:06 +0800

    Edit functionality

--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createNote, deleteNote, getAllNotes, updateNote } from "../controllers/notesController.js";
+import { createNote, deleteNote, getAllNotes, getNoteById, updateNote } from "../controllers/notesController.js";
 
 const router = express.Router();
 
 router.get("/", getAllNotes);
 router.post("/", createNote);
+router.get("/:id", getNoteById);
 router.put("/:id", updateNote);
 router.delete("/:id", deleteNote);
 
--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -1,11 +1,142 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import toast from 'react-hot-toast';
+import { Link, useNavigate, useParams } from 'react-router';
+import api from '../lib/axios';
+import { ArrowLeft, LoaderCircle, Trash2Icon } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Field, FieldLabel } from '@/components/ui/field';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
 
 const NoteDetailPage = () => {
+  const [note, setNote] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [saving, setSaving] = useState(false);
+
+  const navigate = useNavigate();
+
+  const { id } = useParams(); // The id of the note tos be edit
+
+  useEffect(() => {
+    const fetchNote = async () => {
+      try {
+        const res = await api.get(`/notes/${id}`);
+        setNote(res.data);
+      } catch (error) {
+        console.log("Error fetching note details:", error);
+        toast.error('Failed to load note details.', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchNote();
+  }, [id]);
+
+  console.log({ note });
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-muted/40">
+        <LoaderCircle className='animate-spin size-10 text-muted-foreground' />
+      </div>
+    );
+  }
+
+  const handleDelete = async () => {
+    if (!window.confirm("Are you sure you want to delete this note?")) return;
+    try {
+      await api.delete(`/notes/${id}`);
+      toast.success("Note deleted successfully");
+      navigate("/");
+    } catch (error) {
+      toast.error("Failed to delete note.");
+      console.error("Error deleting note:", error);
+    }
+  };
+
+  const handleSave = async () => {
+    if (!note.title.trim() || !note.content.trim()) {
+      toast.error("Title and/or content cannot be empty.");
+      return;
+    }
+
+    setSaving(true);
+
+    try {
+      await api.put(`/notes/${id}`, note);
+      toast.success("Note updated successfully");
+      navigate("/");
+    } catch (error) {
+      toast.error("Failed to update note.");
+      console.error("Error updating note:", error);
+    } finally {
+      setSaving(false);
+    }
+  };
+
   return (
-    <div>
-      Note detail page
-    </div>
+    <div className='min-h-screen flex items-center justify-center bg-muted/40 p-4'>
+      <div className="w-full max-w-xl">
+        <div className="flex items-center justify-between mb-6">
+          <Link
+            to="/"
+            className="inline-flex items-center text-sm font-medium text-muted-foreground hover:text-foreground transition-colors"
+          >
+            <ArrowLeft className='mr-2 size-4' /> Back to Notes
+          </Link>
+          <Button variant="destructive" onClick={handleDelete}>
+            <Trash2Icon className='mr-2 size-4' />
+            {saving && <LoaderCircle className="mr-2 h-4 w-4 animate-spin" />}
+            {saving ? "Deleting..." : "Delete Note"}
+          </Button>
+        </div>
+
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-3xl text-center">Edit Note</CardTitle>
+          </CardHeader>
+          <CardContent className="space-y-4">
+            {/* Title */}
+            <div className="space-y-2">
+              <Field>
+                <FieldLabel htmlFor="input-field-title">Title</FieldLabel>
+                <Input
+                  id="input-field-title"
+                  type="text"
+                  placeholder="Note title"
+                  value={note.title}
+                  onChange={(e) => setNote({ ...note, title: e.target.value })}
+                />
+              </Field>
+            </div>
+
+            {/* Content */}
+            <div className="space-y-2">
+              <Field>
+                <FieldLabel htmlFor="textarea-message">Content</FieldLabel>
+                <Textarea
+                  id="textarea-message"
+                  placeholder="What is your note all about?"
+                  value={note.content}
+                  onChange={(e) => setNote({ ...note, content: e.target.value })}
+                  className="min-h-37.5"
+                />
+              </Field>
+            </div>
+          </CardContent>
+
+          <CardFooter className="flex justify-end">
+            <Button disabled={saving} onClick={handleSave}>
+              {saving && <LoaderCircle className="mr-2 h-4 w-4 animate-spin" />}
+              {saving ? "Saving..." : "Save Changes"}
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
+    </div >
   )
-}
+};
 
 export default NoteDetailPage
